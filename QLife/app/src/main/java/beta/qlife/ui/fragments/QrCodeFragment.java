8536e55cd093cae126914f10ed255c14d28abfbc
Carson Cook <cook.carson38@gmail.com>
2018-07-08 16:51:39 -0400

    generate test qrcode

--- QLife/app/src/main/java/beta/qlife/ui/fragments/QrCodeFragment.java
+++ QLife/app/src/main/java/beta/qlife/ui/fragments/QrCodeFragment.java
@@ -1,5 +1,7 @@
 package beta.qlife.ui.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 import beta.qlife.R;
 import beta.qlife.interfaces.enforcers.ActionbarFragment;
@@ -20,6 +28,20 @@ public class QrCodeFragment extends Fragment implements ActionbarFragment, Drawe
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_qrcode, container, false);
         setActionbarTitle();
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode("hello", BarcodeFormat.QR_CODE, 256, 256);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            ((ImageView) v.findViewById(R.id.qrcode_pic)).setImageBitmap(bmp);
+        } catch (WriterException e) {
+        }
         return v;
     }
 
